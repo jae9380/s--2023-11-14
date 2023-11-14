70f80e7a6e7e780cfd84830a7d68eaef32ed353a
jae9380 <ljy5314@gmail.com>
2023-11-14 17:40:02 +0900

    step7 (List)

--- src/main/java/com/example/sb20231114/ArticleController.java
+++ src/main/java/com/example/sb20231114/ArticleController.java
@@ -6,13 +6,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ArticleController {
-    private Article[] articles = new Article[3];
-    private int articlesLastIndex = -1;
+    private ArrayList<Article> articles = new ArrayList<>();
     @GetMapping("/article/write")
     String showWrite() {
         return "article/write";
@@ -22,21 +23,21 @@ public class ArticleController {
     @ResponseBody
     Map<String,Object> doWrite(String title, String body) {
         Map<String,Object>rs=new HashMap<>();
-        Article article = new Article(articlesLastIndex + 2, title, body);
+        Article article = new Article(articles.size()+1, title, body);
         rs.put("msg", "%d번 게시물이 작성되었습니다.".formatted(article.getId()));
         rs.put("data", article);
-        articles[++articlesLastIndex] = article;
+        articles.add(article);
         return rs;
     }
 
     @GetMapping("/article/getLastArticle")
     @ResponseBody
     Article getLastArticle(String title, String body) {
-        return articles[++articlesLastIndex];
+        return articles.getLast();
     }
     @GetMapping("/article/getArticles")
     @ResponseBody
-    Article[] getArticles() {
+    List<Article> getArticles() {
         return articles;
     }
 }
