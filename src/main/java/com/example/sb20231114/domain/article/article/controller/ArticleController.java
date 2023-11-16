aa8379e1cf368c310935c7a845be5f9a031ff2cb
jae9380 <ljy5314@gmail.com>
2023-11-16 13:23:57 +0900

    정리

--- src/main/java/com/example/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/example/sb20231114/domain/article/article/controller/ArticleController.java
@@ -2,9 +2,6 @@ package com.example.sb20231114.domain.article.article.controller;
 
 import com.example.sb20231114.domain.article.article.entity.Article;
 import com.example.sb20231114.domain.article.article.service.ArticleService;
-import com.example.sb20231114.global.rq.Rq;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
@@ -14,7 +11,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -24,14 +20,38 @@ import java.util.List;
 
 public class ArticleController {
     private final ArticleService articleService;
-    private final Rq rq;
 
-    @GetMapping("/article/delete/{id}")
-    String delete(@PathVariable long id) {
-        articleService.delete(id);
+    @GetMapping("/article/list")
+    String showList(Model model){
+        List<Article>articles=articleService.finAll();
+        model.addAttribute("articles",articles);
+        return "article/list";
+    }
 
-        String msg = "id %d, article deleted".formatted(id);
+    @GetMapping("/article/detail/{id}")
+    String showDetail(Model model, @PathVariable long id) {
+        Article article = articleService.findById(id).get();
+        model.addAttribute("article", article);
+        return "article/detail";
+    }
 
+    @GetMapping("/article/write")
+    String showWrite() {
+        return "article/write";
+    }
+
+    @Data
+    public static class WriteForm {
+        @NotBlank
+        private String title;
+        @NotBlank
+        private String body;
+    }
+
+    @PostMapping("/article/write")
+    String write(@Valid WriteForm writeForm) {
+        Article article = articleService.write(writeForm.title, writeForm.body);
+        String msg = "id %d, article created".formatted(article.getId());
         return "redirect:/article/list?msg=" + msg;
     }
 
@@ -42,79 +62,28 @@ public class ArticleController {
         return "article/modify";
     }
 
-    @GetMapping("/article/detail/{id}")
-    String showDetail(Model model, @PathVariable long id) {
-        Article article = articleService.findById(id).get();
-        model.addAttribute("article", article);
-        return "article/detail";
-    }
-    @GetMapping("/article/write")
-    String showWrite() {
-        return "article/write";
-    }
     @Data
-    public static class WriteForm {
+    public static class ModifyForm {
         @NotBlank
         private String title;
         @NotBlank
         private String body;
     }
-    @PostMapping("/article/write")
-    String write(@Valid WriteForm writeForm) {
-        Article article = articleService.write(writeForm.title, writeForm.body);
-        String msg = "id %d, article created".formatted(article.getId());
+
+    @PostMapping("/article/modify/{id}")
+    String write(@PathVariable long id, @Valid ModifyForm modifyForm) {
+        articleService.modify(id, modifyForm.title, modifyForm.body);
+        String msg = "id %d, article modified".formatted(id);
         return "redirect:/article/list?msg=" + msg;
     }
 
-    @GetMapping("/article/list")
-    String showList(Model model){
-        List<Article>articles=articleService.finAll();
-        model.addAttribute("articles",articles);
-        return "article/list";
-    }
+    @GetMapping("/article/delete/{id}")
+    String delete(@PathVariable long id) {
+        articleService.delete(id);
 
-    @GetMapping("/article/getLastArticle")
-    @ResponseBody
-    Article getLastArticle() {
-        return articleService.findLastArticle();
-    }
-    @GetMapping("/article/getArticles")
-    @ResponseBody
-    List<Article> getArticles() {
-        return articleService.finAll();
-    }
+        String msg = "id %d, article deleted".formatted(id);
 
-    @GetMapping("/article/articleServicePointer")
-    @ResponseBody
-    String articleServicePointer(){
-        return articleService.toString();
-        // 객체를 계속 만들지 않고 하나를 만들어 계속 재활용 하고있다.
+        return "redirect:/article/list?msg=" + msg;
     }
-    @GetMapping("/article/httpServletRequestPointer")
-    @ResponseBody
-    String httpServletRequestPointer(HttpServletRequest req){
-        return req.toString();
-    }
-    // Request와Response는 계속 값이 바뀐다.
-    // 해당 객체는 필요할 떄 잠시 생기고 사라진다.
-    @GetMapping("/article/httpServletResponsePointer")
-    @ResponseBody
-    String httpServletResponsePointer(HttpServletResponse resp){
-        return resp.toString();
-    }
-
-    @GetMapping("/article/rqPointer")
-    @ResponseBody
-    String rqPointer(){
-        return rq.toString();
-    }
-
-    @GetMapping("/article/rqTest")
-    String showRqTest(Model model) {
-        String rqToString = rq.toString();
-        model.addAttribute("rqToString", rqToString);
-        return "article/rqTest";
-    }
-
 }
 
--- src/main/java/com/example/sb20231114/domain/article/article/service/ArticleService.java
+++ src/main/java/com/example/sb20231114/domain/article/article/service/ArticleService.java
@@ -14,7 +14,6 @@ public class ArticleService {
     private final ArticleRepository articleRepository;
 
     public Article write(String title, String body) {
-        // service는 repository에 전달만 하고 순서는 repository가 알아서
         Article article=new Article(title,body);
         articleRepository.save(article);
         return article;
@@ -34,5 +33,11 @@ public class ArticleService {
 
     public void delete(long id) {
         articleRepository.delete(id);
+    }
+
+    public void modify(long id, String title, String body) {
+        Article article = findById(id).get();
+        article.setTitle(title);
+        article.setBody(body);
     }
 }
