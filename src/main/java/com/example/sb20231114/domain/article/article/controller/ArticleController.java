741fc1890f4e32470f0a15d9122e59c59f13327b
jae9380 <ljy5314@gmail.com>
2023-11-16 09:54:33 +0900

    step51/ Case1

--- src/main/java/com/example/sb20231114/domain/article/article/controller/ArticleController.java
+++ src/main/java/com/example/sb20231114/domain/article/article/controller/ArticleController.java
@@ -31,6 +31,19 @@ public class ArticleController {
     @PostMapping("/article/write")
     @ResponseBody
     RsData write(String title, String body) {
+        if (title == null || title.trim().length() == 0) {
+            return new RsData<>(
+                    "F-1",
+                    "제목을 입력해주세요."
+            );
+        }
+
+        if (body == null || body.trim().length() == 0) {
+            return new RsData<>(
+                    "F-2",
+                    "내용을 입력해주세요."
+            );
+        }
         Article article=articleService.write(title,body);
         RsData<Article> rs = new RsData<>(
                 "S-1",
--- src/main/java/com/example/sb20231114/global/rsData/RsData.java
+++ src/main/java/com/example/sb20231114/global/rsData/RsData.java
@@ -2,10 +2,12 @@ package com.example.sb20231114.global.rsData;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @AllArgsConstructor
+@RequiredArgsConstructor
 @Getter
 public class RsData<T> {
-    private String resultCode, msg;
+    private final String resultCode, msg;
     private T data;
 }
