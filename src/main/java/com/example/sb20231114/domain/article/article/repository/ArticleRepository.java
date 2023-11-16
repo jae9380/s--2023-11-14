fc7f0547e7f84670f648e1c81774d3b31e71dd15
jae9380 <ljy5314@gmail.com>
2023-11-16 11:13:54 +0900

    step65/ testData

--- src/main/java/com/example/sb20231114/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/example/sb20231114/domain/article/article/repository/ArticleRepository.java
@@ -4,17 +4,17 @@ import com.example.sb20231114.domain.article.article.entity.Article;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 @RequiredArgsConstructor
 @Repository // @Service와 같은 의미
 // @Component을 적어도 차이는 없다.
 public class ArticleRepository {
-//    ArticleArrayList를 직접 만들어서 상속받으면 가능하지만 번거러운 작업이 된다.
-    private final List<Article> articles;
-// @Bean을 2개를 만들었다.
-// 기존의 @Bean을 이름을 바꾸었다.
-// 그러면 기존에는 LinkedList를 사용했지만, 이번에는 ArrayList를 사용한다.
-//
+    private final List<Article> articles = new ArrayList<>(){{
+        add(new Article(1L, "제목 1", "내용 1"));
+        add(new Article(2L, "제목 2", "내용 2"));
+        add(new Article(3L, "제목 3", "내용 3"));
+    }};
     public Article save(Article article) {
         if (article.getId()==null){
             article.setId(articles.size()+1L);
--- src/main/java/com/example/sb20231114/global/app/AppConfig.java
+++ src/main/java/com/example/sb20231114/global/app/AppConfig.java
@@ -1,21 +1,9 @@
 package com.example.sb20231114.global.app;
 
-import com.example.sb20231114.domain.article.article.entity.Article;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    List<Article> articles2(){
-        return new java.util.LinkedList<>();
-    }
 
-    @Bean
-    List<Article> articles(){
-        return new java.util.ArrayList<>();
-    }
 
 }
