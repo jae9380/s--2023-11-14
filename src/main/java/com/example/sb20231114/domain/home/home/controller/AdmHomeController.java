c66993bede925929f0c987048ec34d8cbdc11868
jae9380 <ljy5314@gmail.com>
2023-11-21 23:40:19 +0900

    step20

--- src/main/java/com/example/sb20231114/domain/home/home/controller/AdmHomeController.java
+++ src/main/java/com/example/sb20231114/domain/home/home/controller/AdmHomeController.java
@@ -2,6 +2,7 @@ package com.example.sb20231114.domain.home.home.controller;
 
 import com.example.sb20231114.global.rq.Rq;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -10,13 +11,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AdmHomeController {
     private final Rq rq;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/adm")
     public String showMain() {
 
 
         return "home/home/adm/main";
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/adm/home/about")
     public String showAbout() {
 
