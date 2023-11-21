41f1708eda8eedb8db355561fea3288c6a5d2d52
jae9380 <ljy5314@gmail.com>
2023-11-21 23:17:57 +0900

    step12

--- src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
@@ -3,6 +3,8 @@ package com.example.sb20231114.global.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,5 +14,10 @@ public class SecurityConfig {
         return http.formLogin(formLogin->formLogin.loginPage("/member/login")
                 .loginProcessingUrl("/member/login").usernameParameter("username")
                 .passwordParameter("password").defaultSuccessUrl("/article/list")).build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
