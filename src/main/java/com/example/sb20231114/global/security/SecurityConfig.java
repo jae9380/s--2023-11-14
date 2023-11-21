661afb45c8640e4571c479199d91361c98e5c9df
jae9380 <ljy5314@gmail.com>
2023-11-21 17:57:20 +0900

    step6

--- src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
         return http.build();
     }
 }
