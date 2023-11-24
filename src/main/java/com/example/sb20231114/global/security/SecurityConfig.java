bef324ed4a5a078133e88419e7d7997aeafda151
jae9380 <ljy5314@gmail.com>
2023-11-24 17:32:51 +0900

    step1/ jpa, h2

--- src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
+++ src/main/java/com/example/sb20231114/global/security/SecurityConfig.java
@@ -6,12 +6,20 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
 public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.authorizeHttpRequests(
+        return http.csrf(csrf -> csrf.ignoringRequestMatchers("/h2-console/**"))
+                .headers(headers -> headers.addHeaderWriter(
+                            new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                            )
+                        )
+                )
+                .authorizeHttpRequests(
                 authorizeHttpRequests -> authorizeHttpRequests
                         .requestMatchers("/adm/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
