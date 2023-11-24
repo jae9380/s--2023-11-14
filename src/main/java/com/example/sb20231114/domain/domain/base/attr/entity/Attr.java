e46734d1acb1164eff697c6cc6197685a170ea22
jae9380 <ljy5314@gmail.com>
2023-11-24 17:51:56 +0900

    step2

---
+++ src/main/java/com/example/sb20231114/domain/domain/base/attr/entity/Attr.java
@@ -0,0 +1,10 @@
+package com.example.sb20231114.domain.domain.base.attr.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Attr {
+    @Id
+    private Long id;
+}
